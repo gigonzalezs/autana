0c8ef268d433fb792459b9e3c96ef912e2d383aa
GB <gb@iunigo.com>
2018-07-16 21:33:03 -0300

    first commit

---
+++ src/main/java/org/arepoframework/demo/ArepoDemoApplication.java
@@ -0,0 +1,12 @@
+package org.arepoframework.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArepoDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArepoDemoApplication.class, args);
+	}
+}
